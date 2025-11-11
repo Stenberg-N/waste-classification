6217bb45f3b5fd9ad80a71d24ed0f19da30754ff
Niko! <niko.stenberg@protonmail.com>
2025-11-11 16:20:38 +0200

    Fix: Use correct model for image prediction     - Fixed a typo that prevented the model from running     - Changed the confidence value to a percentage

--- src/evaluate.py
+++ src/evaluate.py
@@ -43,7 +43,7 @@ def predict_image(image_path, checkpoint_path=(MODELS_DIR / 'stage2_best.pth'), 
     classes = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = get_model(model_name='mobilenetv4_conv_small', num_classes=6)
+    model = get_model(model_name='mobilenetv4_hybrid_medium.e500_r224_in1k', num_classes=6)
     model.load_state_dict(torch.load(checkpoint_path, weights_only=True))
     model.to(device)
     model.eval()
@@ -54,7 +54,7 @@ def predict_image(image_path, checkpoint_path=(MODELS_DIR / 'stage2_best.pth'), 
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
-    image = Image.open(image_path).convert('RGG')
+    image = Image.open(image_path).convert('RGB')
     image_tensor = transform(image).unsqueeze(0).to(device)
 
     with torch.no_grad():
@@ -63,7 +63,7 @@ def predict_image(image_path, checkpoint_path=(MODELS_DIR / 'stage2_best.pth'), 
         confidence = torch.softmax(outputs, dim=1)[0][predicted].item()
 
     predicted_class = classes[predicted.item()]
-    print(f'Predicted: {predicted_class} (Confidence: {confidence:.2f})')
+    print(f'Predicted: {predicted_class} (Confidence: {confidence * 100:.2f}%)')
     return predicted_class, confidence
 
 if __name__ == '__main__':
